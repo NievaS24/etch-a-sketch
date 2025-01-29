929f8a47c917bfeaa04eae3eded99c051793a798
NievaS24 <santinieva24@gmail.com>
2025-01-29 13:20:03 -0300

    feat: :sparkles: Pregunta al usuario por el tamaño de la grilla
    Se creo un boton que al tocarlo permite al usuario ingresar un numero positivo menor o igual a 100. Se borra la grilla anterior y se genera una nueva grilla con el tamaño ingresado.

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 const container = document.querySelector("#container");
-
-for (let i = 1; i <= 256; i++) {
-    const div = document.createElement("div");
-    div.setAttribute("id", "div" + i);
-    div.style.width = "6vw";
-    div.style.height = "6vw";
-    container.appendChild(div);
-    div.addEventListener("mouseover", () => {
-        const randomRed = Math.floor(Math.random() * 255);
+function changeColor (div) {
+    const randomRed = Math.floor(Math.random() * 255);
         const randomGreen = Math.floor(Math.random() * 255);
         const randomBlue = Math.floor(Math.random() * 255);
         div.style.backgroundColor = "rgb(" + randomRed + ", " + randomGreen + ", " + randomBlue + ")";
-    })
 }
+let width = 1000 / 16;
+for (let i = 1; i <= 256; i++) {
+    const div = document.createElement("div");
+    div.setAttribute("id", "div" + i);
+    div.style.width = width + "px";
+    div.style.height = width + "px";
+    container.appendChild(div);
+    div.addEventListener("mouseover", () => changeColor(div));
+}
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+    let number;
+    do {       
+        number = parseInt(prompt("Ingrese el numero de cuadrados por lados"));
+    } while (number <= 0 ||number > 100 || !(Number.isInteger(number)));
+    
+    let i = 1;
+    let div = document.querySelector("#div" + i);
+    while (div != null) {
+        container.removeChild(div);
+        i++;
+        div = document.querySelector("#div" + i);
+    }
+    width = (1000 / number) ;
 
-// container.addEventListener("mouseenter", (event) => {
-//     let div = event.target.id
-//     div.style.backgroundColor = "red";
-// })
+    for (let i = 1; i<= (number * number); i++) {
+        const div = document.createElement("div");
+        div.setAttribute("id", "div" + i);
+        div.style.width = width + "px";
+        div.style.height = width + "px";
+        container.appendChild(div);
+        div.addEventListener("mouseover", () => changeColor(div));
+    }
+})
